4bdf0c39fa9d04e2d97f5f7731f6f2ba60628296
JasonYou <qingwei0713@163.com>
2020-01-13 14:45:30 +0800

    feat: init controllBar

--- src/containers/controllBar.tsx
+++ src/containers/controllBar.tsx
@@ -1,4 +1,3 @@
-// 主页、聊天、我
 import React, { Component } from 'react';
 import {
   Icon
@@ -6,18 +5,30 @@ import {
 import '../assets/scss/controllBar.scss'
 
 class ControllBar extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+  toHome() {
+    window.location.href = '#/home';
+  }
+  toChat() {
+    window.location.href = '#/chat';
+  }
+  toSetting() {
+    window.location.href = '#/settings';
+  }
   render() {
     return (
       <div className="za-controll-bar">
-        <div className="controll-bar-item">
+        <div className="controll-bar-item" onClick={this.toHome}>
           <Icon className="bar-icon" type="home" />
           <div>主页</div>
         </div>
-        <div className="controll-bar-item">
+        <div className="controll-bar-item" onClick={this.toChat}>
           <Icon className="bar-icon" type="message" />
           <div>聊天</div>
         </div>
-        <div className="controll-bar-item">
+        <div className="controll-bar-item" onClick={this.toSetting}>
           <Icon className="bar-icon" type="user" />
           <div>我</div>
         </div>
