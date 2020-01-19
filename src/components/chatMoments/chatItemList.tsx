27b6e49cdecd70b0e457c656b3dbe1b6fdf8dd32
jason <qingwei0713@163.com>
2020-01-19 22:59:29 +0800

    feat: Chat框架布局

--- src/components/chatMoments/chatItemList.tsx
+++ src/components/chatMoments/chatItemList.tsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import {
+  List
+} from 'antd-mobile';
 
+const ChatItem = List.Item;
 // 可收缩
 class ChatItemList extends Component {
   getChatItem = () => {
     return (
-      <div></div>
+      <div>
+        <ChatItem>
+          <div>头像</div>
+          <div>内容</div>
+          <div>时间</div>
+        </ChatItem>
+      </div>
     )
   }
   render() {
     return (
-      <div>聊天信息列表</div>
+      <div>聊天信息列表</div>,
+      getChatItem()
     )
   }
 }
--- src/components/chatMoments/chatWindow.tsx
+++ src/components/chatMoments/chatWindow.tsx
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
 import ChatBar from './chatBar';
-import ChatLeftMenu from './chatLeftMenu';
 import ChatInputArea from './chatInputArea';
 import ChatHeader from './chatHeader';
-import ChatItemList from './chatItemList';
 import ChatSubmit from './chatSubmit';
 import ChatInfoList from './chatInfoList';
-import PersonalInfo from './personalInfo';
 
 class GroupChat extends Component {
   render() {
     return (
       <>
-        <ChatLeftMenu></ChatLeftMenu>
-        <ChatItemList></ChatItemList>
-        <div>
-          <ChatHeader></ChatHeader>
-          <ChatInfoList></ChatInfoList>
-          <ChatBar></ChatBar>
-          <ChatInputArea></ChatInputArea>
-          <ChatSubmit></ChatSubmit>
-        </div>
-        <PersonalInfo></PersonalInfo>
+        <ChatHeader></ChatHeader>
+        <ChatInfoList></ChatInfoList>
+        <ChatBar></ChatBar>
+        <ChatInputArea></ChatInputArea>
+        <ChatSubmit></ChatSubmit>
       </>
     )
   }
